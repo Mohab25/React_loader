3cad988b41bb0ef38260d7acad99840ed83ec0b7
mohab <homab3@gmail.com>
2020-07-17 16:15:31 +0200

    Updates

---
+++ precache-manifest.f4732421a671941ca6ff3387ce155832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7441cf86ab25ad581731de7a30ce5e3b",
+    "url": "/React_loader/index.html"
+  },
+  {
+    "revision": "12bc20993d7546816b34",
+    "url": "/React_loader/static/css/main.3c12f536.chunk.css"
+  },
+  {
+    "revision": "d36ddeed411276b0e42c",
+    "url": "/React_loader/static/js/2.5da1b3fa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_loader/static/js/2.5da1b3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12bc20993d7546816b34",
+    "url": "/React_loader/static/js/main.7b681667.chunk.js"
+  },
+  {
+    "revision": "a843e815c2333175cf85",
+    "url": "/React_loader/static/js/runtime-main.05462416.js"
+  },
+  {
+    "revision": "8d4a71ba0edb1965972782a4a6b237a8",
+    "url": "/React_loader/static/media/bg.8d4a71ba.jpg"
+  }
+]);
